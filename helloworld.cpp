75a5488f65a8d981e8b69b67161e22063ebed4af
VishnuAnilkumar <vishnu.pullaikodi@gmail.com>
2024-02-07 17:44:20 -0800

    test again

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,8 +4,6 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
-    cout << "have a nice ass day" << endl;
-    cout<<"have a nicest ass day"<<endl;
     cout << "hello everyone" << endl;
     cout << "Hello" << endl;
     return 0;
